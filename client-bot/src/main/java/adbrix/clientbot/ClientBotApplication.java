76d994f913dfe79ce146e3acfb9bdda5a3a41cdf
nopecho <ch.jooon@gmail.com>
2022-04-09 13:45:12 +0900

    client-bot init

---
+++ client-bot/src/main/java/adbrix/clientbot/ClientBotApplication.java
@@ -0,0 +1,13 @@
+package adbrix.clientbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientBotApplication.class, args);
+	}
+
+}
---
+++ client-bot/src/test/java/adbrix/clientbot/ClientBotApplicationTests.java
@@ -0,0 +1,13 @@
+package adbrix.clientbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
