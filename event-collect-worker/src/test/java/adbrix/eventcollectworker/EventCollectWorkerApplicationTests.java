cc42b0234441369056f73cfb8670a8f5eab474cd
nopecho <ch.jooon@gmail.com>
2022-04-11 18:18:29 +0900

    event-worker init

---
+++ event-collect-worker/src/test/java/adbrix/eventcollectworker/EventCollectWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package adbrix.eventcollectworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventCollectWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
